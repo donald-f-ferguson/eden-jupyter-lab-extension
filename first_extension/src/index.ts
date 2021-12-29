031dbd2927d0fbdeafae480fa236c2e8a5378fcc
donaldferguson <donff2@aol.com>
2021-12-29 10:23:37 -0500

    Show Astronomy Picture command in palette

--- first_extension/src/index.ts
+++ first_extension/src/index.ts
@@ -3,9 +3,50 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+
+import { Widget } from '@lumino/widgets';
+
 /**
  * Initialization data for the first-extension extension.
  */
+
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'first-extensuon',
+  autoStart: true,
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    console.log(
+      '\n******** JupyterLab extension first_extension is activated! *********\n'
+    );
+
+    // Create a blank content widget inside of a MainAreaWidget
+    const content = new Widget();
+    const widget = new MainAreaWidget({ content });
+    widget.id = 'first_extension';
+    widget.title.label = 'Astronomy Picture';
+    widget.title.closable = true;
+
+    // Add an application command
+    const command = 'first_extension:open';
+    app.commands.addCommand(command, {
+      label: 'Random Astronomy Picture',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({ command, category: 'Tutorial' });
+  }
+};
+
+/*
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'first-extension:plugin',
   autoStart: true,
@@ -13,5 +54,6 @@ const plugin: JupyterFrontEndPlugin<void> = {
     console.log('JupyterLab extension first-extension is activated!');
   }
 };
+*/
 
-export default plugin;
+export default extension;
