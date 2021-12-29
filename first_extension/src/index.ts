91fc82c657fd8d637271ce1f0df079ae47de8a77
donaldferguson <donff2@aol.com>
2021-12-29 10:32:38 -0500

    Completed first pass at extension adding image!

--- first_extension/src/index.ts
+++ first_extension/src/index.ts
@@ -7,6 +7,15 @@ import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
 
 import { Widget } from '@lumino/widgets';
 
+interface APODResponse {
+  copyright: string;
+  date: string;
+  explanation: string;
+  media_type: 'video' | 'image';
+  title: string;
+  url: string;
+};
+
 /**
  * Initialization data for the first-extension extension.
  */
@@ -15,7 +24,7 @@ const extension: JupyterFrontEndPlugin<void> = {
   id: 'first-extensuon',
   autoStart: true,
   requires: [ICommandPalette],
-  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+  activate: async (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log(
       '\n******** JupyterLab extension first_extension is activated! *********\n'
     );
@@ -26,6 +35,30 @@ const extension: JupyterFrontEndPlugin<void> = {
     widget.id = 'first_extension';
     widget.title.label = 'Astronomy Picture';
     widget.title.closable = true;
+
+    // Add an image element to the content
+    let img = document.createElement('img');
+    content.node.appendChild(img);
+
+    // Get a random date string in YYYY-MM-DD format
+    function randomDate() {
+      const start = new Date(2010, 1, 1);
+      const end = new Date();
+      const randomDate = new Date(start.getTime() + Math.random()*(end.getTime() - start.getTime()));
+      return randomDate.toISOString().slice(0, 10);
+    }
+
+    // Fetch info about a random picture
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${randomDate()}`);
+    const data = await response.json() as APODResponse;
+
+    if (data.media_type === 'image') {
+      // Populate the image
+      img.src = data.url;
+      img.title = data.title;
+    } else {
+      console.log('Random APOD was not a picture.');
+    }
 
     // Add an application command
     const command = 'first_extension:open';
